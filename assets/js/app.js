cdff6bf6c5b4b0f6e600ff4fb242b6f89a1d951c
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2022-09-09 22:02:57 +0600

    first input

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:26,
+    nav:false,
+    smartSpeed:700,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:2,
+            center:true,
+        }
+    }
+})
